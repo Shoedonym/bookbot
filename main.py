192a0292c51deaca9171a93983769be4a1abb220
Shoedonym <disasta121@hotmail.com>
2025-05-02 12:31:53 -0400

    Now pretty! And sorted!

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
+import sys
 from stats import word_count
 from stats import char_count
+from stats import list_sorter
 
+#imports book text from file
 def get_book_text(filepath):
     with open(filepath) as book:
         return book.read()
 
 def main():
-    booktext = get_book_text('./books/frankenstein.txt')
+    try:
+        book_path = sys.argv[1]
+    except Exception as e:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    booktext = get_book_text(book_path)
     num_words = word_count(booktext)
     num_chars = char_count(booktext)
-    print(f"{num_words} words found in the document")
-    print(num_chars)
+    character_count = list_sorter(num_chars)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for character in character_count:
+        if character["char"].isalpha() == True:
+            print(f"{character["char"]}: {character["count"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,7 +1,9 @@
+#counts the number of words in the book
 def word_count(bookspam):
     words = bookspam.split()
     return len(words)
 
+#counts the amount of times each individual character appears in the book and returns as a dictionary
 def char_count(booksplosion):
     book_chars = list(booksplosion.lower())
     char_dict = {}
@@ -11,3 +13,16 @@ def char_count(booksplosion):
         else:
             char_dict[char] = 1
     return char_dict
+
+#sorting key (by count integer)
+def sort_on(dict):
+    return dict["count"]
+
+#accepts the dictionary provided by char_count and sorts it as a list of dictionaries by count in decending order
+def list_sorter(unsorted_mess):
+    sorted_mess = []
+    for char in unsorted_mess:
+        count = unsorted_mess[char]
+        sorted_mess.append({"char": char, "count": count})
+    sorted_mess.sort(reverse=True, key=sort_on)
+    return sorted_mess
